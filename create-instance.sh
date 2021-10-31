f4d29cca4f6fed4f7201a855bcb7a55e84b038bf
Manu Reddy <reddybmanohar@gmail.com>
2021-10-31 23:29:30 +0530

    create-instance--ansible

--- create-instance.sh
+++ create-instance.sh
@@ -4,7 +4,7 @@ CREATE() {
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 
   if [ $COUNT -eq 0 ]; then
-    aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro --security-group-ids sg-09fb885e47a86a40e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
+    aws ec2 run-instances --image-id ami-0e4e4b2f188e91845 --instance-type t2.micro --security-group-ids sg-0f65da430b2ca3fb9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq &>/dev/null
   else
     echo  -e "\e[1;33m$1 Instance already exists\e[0m"
     return
@@ -16,7 +16,7 @@ CREATE() {
 
   sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
 
-  aws route53 change-resource-record-sets --hosted-zone-id Z042241811DLHPXKSVOGM --change-batch file:///tmp/record.json | jq &>/dev/null
+  aws route53 change-resource-record-sets --hosted-zone-id Z0371024O5MD9RNOBWLB --change-batch file:///tmp/record.json | jq &>/dev/null
 }
 
 if [ "$1" == "all" ]
