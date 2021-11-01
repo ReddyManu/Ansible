bba9845441e8de279f8255b39f6bf1f8b795c3d7
Manu Reddy <reddybmanohar@gmail.com>
2021-11-01 19:26:49 +0530

    create-instance DNS record update

--- create-instance.sh
+++ create-instance.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+UPDATE_DNS_RECORDS() {
+
+  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+
+  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+
+  aws route53 change-resource-record-sets --hosted-zone-id Z0371024O5MD9RNOBWLB --change-batch file:///tmp/record.json | jq &>/dev/null
+}
+
 CREATE() {
   COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 
@@ -12,11 +21,8 @@ CREATE() {
 
   sleep 5
 
-  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
+  UPDATE_DNS_RECORDS $1
 
-  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-
-  aws route53 change-resource-record-sets --hosted-zone-id Z0371024O5MD9RNOBWLB --change-batch file:///tmp/record.json | jq &>/dev/null
 }
 
 if [ "$1" == "all" ]
