0bbdd3e1c3398f1c2aae10384b6980c65bd315f8
Manu Reddy <reddybmanohar@gmail.com>
2021-11-01 18:35:07 +0530

    create-instance

--- create-instance.sh
+++ create-instance.sh
@@ -14,7 +14,7 @@ CREATE() {
 
   IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
 
-  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+  sed -e "s/DNSNAME/$1-dev.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
 
   aws route53 change-resource-record-sets --hosted-zone-id Z0371024O5MD9RNOBWLB --change-batch file:///tmp/record.json | jq &>/dev/null
 }
